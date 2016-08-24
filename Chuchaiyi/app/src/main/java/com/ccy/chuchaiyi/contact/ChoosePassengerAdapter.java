a339bb2733902c050089b6c3a0682dc5436ad446
11hhlin1 <11hhlin1@gmail.com>
2016-08-24 17:51:33 +0800

    sdad

---
+++ Chuchaiyi/app/src/main/java/com/ccy/chuchaiyi/contact/ChoosePassengerAdapter.java
@@ -0,0 +1,9 @@
+package com.ccy.chuchaiyi.contact;
+
+import com.ccy.chuchaiyi.base.BaseRecyclerViewAdapter;
+
+/**
+ * Created by Chuck on 2016/8/24.
+ */
+public class ChoosePassengerAdapter extends BaseRecyclerViewAdapter<>{
+}
---
+++ Chuchaiyi/app/src/main/java/com/ccy/chuchaiyi/contact/PassengerData.java
@@ -0,0 +1,10 @@
+package com.ccy.chuchaiyi.contact;
+
+import com.ccy.chuchaiyi.base.BaseRecyclerItemData;
+
+/**
+ * Created by Chuck on 2016/8/24.
+ */
+public class PassengerData extends BaseRecyclerItemData{
+
+}
