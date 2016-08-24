8a352b939e6fb0afa1c6d34aa0ec44bcb11330db
11hhlin1 <11hhlin1@gmail.com>
2016-08-24 17:52:06 +0800

    dsad

--- Chuchaiyi/app/src/main/java/com/ccy/chuchaiyi/contact/ChoosePassengerAdapter.java
+++ Chuchaiyi/app/src/main/java/com/ccy/chuchaiyi/contact/ChoosePassengerAdapter.java
@@ -1,9 +1,16 @@
 package com.ccy.chuchaiyi.contact;
 
+import android.content.Context;
+
 import com.ccy.chuchaiyi.base.BaseRecyclerViewAdapter;
+
+import java.util.List;
 
 /**
  * Created by Chuck on 2016/8/24.
  */
-public class ChoosePassengerAdapter extends BaseRecyclerViewAdapter<>{
+public class ChoosePassengerAdapter extends BaseRecyclerViewAdapter<PassengerData>{
+    public ChoosePassengerAdapter(Context context, List<PassengerData> items) {
+        super(context, items);
+    }
 }
