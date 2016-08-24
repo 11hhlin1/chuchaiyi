f8df802e9a509ef89e59d9adbaaf50bb4d99f26f
11hhlin1 <11hhlin1@gmail.com>
2016-08-24 18:33:42 +0800

    PreferencesManager

--- Chuchaiyi/applibrary/src/main/java/com/gjj/applibrary/util/PreferencesManager.java
+++ Chuchaiyi/applibrary/src/main/java/com/gjj/applibrary/util/PreferencesManager.java
@@ -142,48 +142,20 @@ public class PreferencesManager {
 
 
 
+
 	/**
 	 * 直接存放对象，反射将根据对象的属性作为key，并将对应的值保存。
-	 * 
+	 *
 	 * @param t
 	 */
 	@SuppressWarnings("rawtypes")
 	public <T> void put(T t) {
 		try {
-			String methodName = "";
-			String saveValue = "";
-			String fieldName = "";
 			Editor edit = preferences.edit();
-			Class cls = t.getClass();
-
-			if (edit != null) {
-				
-				Method[] methods = cls.getDeclaredMethods();
-				Field[] fields = cls.getDeclaredFields();
-				
-				for (Method method : methods) {
-					methodName = method.getName();
-					for (Field f : fields) {
-						fieldName = f.getName();
-						if (methodName.toLowerCase().contains(fieldName.toLowerCase())) {
-
-							Object value = method.invoke(t);
-							if (value != null && !TextUtils.isEmpty(String.valueOf(value))) {
-								saveValue = String.valueOf(value);
-							}
-
-							edit.putString(fieldName, String.valueOf(saveValue));
-							break;
-						}
-					}
-				}
-				edit.commit();
-			}
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
+			String json = JsonMananger.beanToJson(t);
+			edit.putString(t.getClass().getSimpleName(), json);
+			edit.commit();
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -216,34 +188,26 @@ public class PreferencesManager {
 		return preferences.getStringSet(key, defValue);
 	}
 
+
 	/**
-	 * 获取整个对象，跟put(T t)对应使用， 利用反射得到对象的属性，然后从preferences获取
-	 * 
+	 * 获取整个对象，跟put(T t)对应使用
+	 *
 	 * @param cls
 	 * @return
 	 */
 	public <T> Object get(Class<T> cls) {
 		Object obj = null;
-		String fieldName = "";
 		try {
-			obj = cls.newInstance();
-			Field[] fields = cls.getDeclaredFields();
-			for (Field f : fields) {
-				fieldName = f.getName();
-				if (!"serialVersionUID".equals(fieldName)) {
-					f.setAccessible(true);
-					f.set(obj, get(f.getName()));
-				}
+			String json = preferences.getString(cls.getClass().getSimpleName(), "");
+			if(!TextUtils.isEmpty(json)){
+				obj = JsonMananger.jsonToBean(json, cls);
 			}
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return obj;
 	}
+
 
 	public int getTheme(int defThemeId) {
 		return instance.get(THEME, defThemeId);
